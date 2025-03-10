2d0dce9835255fe07429d0fca3e1de76c9f6dee7
arrictan <tanarriclance@gmail.com>
2025-03-10 11:13:29 +0800

    add: initial camera and player movement

---
+++ Assets/Scripts/Player/PlayerCameraFollowIsometric.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class PlayerCameraFollowIsometric : MonoBehaviour
+{
+	[SerializeField]
+	private Transform _target;
+	
+	private Vector3 _defaultPosition;
+	private Vector3 _offset;
+	
+	#region [ Unity Overrideable Methods ]
+	
+	private void Awake()
+	{
+		if ( _target == null )
+		{
+			Debug.LogWarningFormat( "PlayerCameraFollowIsometric: Target player is not set, camera won't follow." );
+			return;
+		}
+		
+		Vector3 currentPos = transform.position;
+		_offset = currentPos - _target.position;
+		_defaultPosition = currentPos;
+	}
+	
+	private void FixedUpdate()
+	{
+		if ( _target == null )
+		{
+			return;
+		}
+		
+		transform.position = _target.position + _offset;
+	}
+	
+	#endregion // Unity Overrideable Methods
+	
+	#region [ Debug ]
+	
+	[ContextMenu( "Reset Position" )]
+	public void ResetPosition()
+	{
+		transform.position = _defaultPosition;
+	}
+	
+	#endregion // Debug
+}
---
+++ Assets/Scripts/Player/PlayerMovementController.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+	[SerializeField]
+	private Rigidbody _rigidBody;
+	
+	[SerializeField]
+	private float _speed = 5;
+	
+	[SerializeField]
+	private float _turnSpeed = 360;
+	
+	private Vector3 _input;
+	
+	private void Update()
+	{
+		_input = new Vector3( Input.GetAxisRaw( "Horizontal" ), 0, Input.GetAxisRaw( "Vertical" ) );
+		
+		if ( _input == Vector3.zero )
+		{
+			return;
+		}
+		
+		Vector3 currentPos = transform.position;
+		var matrix = Matrix4x4.Rotate( Quaternion.Euler( 0, 45, 0 ) );
+		var skewedInput = matrix.MultiplyPoint3x4( _input );
+		var relative = ( currentPos + skewedInput ) - currentPos;
+		var rotation = Quaternion.LookRotation( relative, Vector3.up );
+		transform.rotation = Quaternion.RotateTowards( transform.rotation, rotation, _turnSpeed * Time.deltaTime );
+	}
+	
+	private void FixedUpdate()
+	{
+		Transform currentTransform = transform;
+		_rigidBody.MovePosition( currentTransform.position + ( currentTransform.forward * _input.magnitude ) * _speed * Time.fixedDeltaTime );
+	}
+}
